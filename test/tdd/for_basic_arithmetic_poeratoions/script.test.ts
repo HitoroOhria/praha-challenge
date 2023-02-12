1974474b545956840cd8ab0222da727e703ed073
ohira.hiroto <hiroto100114@gmail.com>
2023-02-12 16:10:24 +0900

    Add divide test.

--- test/tdd/for_basic_arithmetic_poeratoions/script.test.ts
+++ test/tdd/for_basic_arithmetic_poeratoions/script.test.ts
@@ -9,6 +9,10 @@ describe("jest test", () => {
 
 describe("calculate", () => {
   describe("add", () => {
+    test("0 を渡すと、0 が返る", () => {
+      expect(calculate("add", 0)).toBe(0);
+    });
+
     test("0 と 0 を渡すと、0 が返る", () => {
       expect(calculate("add", 0, 0)).toBe(0);
     });
@@ -25,6 +29,16 @@ describe("calculate", () => {
       expect(calculate("add", 1, 1)).toBe(2);
     });
 
+    test("1 と 1 と 1 を渡すと、3 が返る", () => {
+      expect(calculate("add", 1, 1, 1)).toBe(3);
+    });
+
+    test("1 ~ 30 まで 30個 の数値を渡すと、465 が返る", () => {
+      const numbers = [...Array(30)].map((_, i) => i + 1);
+
+      expect(calculate("add", ...numbers)).toBe(465);
+    });
+
     test("999 と 1 を渡すと、1000 が返る", () => {
       expect(calculate("add", 999, 1)).toBe(1000);
     });
@@ -32,11 +46,45 @@ describe("calculate", () => {
     test("1000 と 1 を渡すと、big big number が返る", () => {
       expect(calculate("add", 1000, 1)).toBe("big big number");
     });
+  });
 
-    test("1 ~ 30 まで 30個 の数値を渡すと、465 が返る", () => {
+  describe("divide", () => {
+    test("0 を渡すと、0 が返る", () => {
+      expect(calculate("divide", 0)).toBe(0);
+    });
+
+    test("0 と 0 を渡すと、0 が返る", () => {
+      expect(calculate("divide", 0, 0)).toBe(0);
+    });
+
+    test("0 と 1 を渡すと、negative number が返る", () => {
+      expect(calculate("divide", 0, 1)).toBe("negative number");
+    });
+
+    test("1 と 0 を渡すと、1 が返る", () => {
+      expect(calculate("divide", 1, 0)).toBe(1);
+    });
+
+    test("1 と 1 を渡すと、0 が返る", () => {
+      expect(calculate("divide", 1, 1)).toBe(0);
+    });
+
+    test("3 と 1 と 1 を渡すと、1 が返る", () => {
+      expect(calculate("divide", 3, 1, 1)).toBe(1);
+    });
+
+    test("1 ~ 30 まで 30個 の数値を渡すと、negative number が返る", () => {
       const numbers = [...Array(30)].map((_, i) => i + 1);
 
-      expect(calculate("add", ...numbers)).toBe(465);
+      expect(calculate("divide", ...numbers)).toBe("negative number");
+    });
+
+    test("1001 と 1 を渡すと、1000 が返る", () => {
+      expect(calculate("divide", 1001, 1)).toBe(1000);
+    });
+
+    test("1002 と 1 を渡すと、1001 が返る", () => {
+      expect(calculate("divide", 1002, 1)).toBe(1001);
     });
   });
 });
--- test/tdd/for_basic_arithmetic_poeratoions/script.ts
+++ test/tdd/for_basic_arithmetic_poeratoions/script.ts
@@ -4,7 +4,24 @@ export const calculate = (
   kind: CalculationKind,
   ...numbers: number[]
 ): number | string => {
-  const result = numbers.reduce((pre, cur) => pre + cur, 0);
+  switch (kind) {
+    case "add":
+      return add(...numbers);
+    case "divide":
+      return divide(...numbers);
+    default:
+      return 0;
+  }
+};
+
+const add = (...numbers: number[]): number | string => {
+  const result = numbers.reduce((pre, cur) => pre + cur);
 
   return result > 1000 ? "big big number" : result;
+};
+
+const divide = (...numbers: number[]): number | string => {
+  const result = numbers.reduce((pre, cur) => pre - cur);
+
+  return result < 0 ? "negative number" : result;
 };
